e37d29cc3b3f556d281474e707913e9894f49816
Chandrashekar1404 <chandrashekar7414@gmail.com>
2025-08-04 19:55:04 +0530

    Use environment variable placeholders for Render

---
+++ src/main/java/com/IEP_Project/StudentManagementSystem/config/CorseConfig.java
@@ -0,0 +1,22 @@
+package com.IEP_Project.StudentManagementSystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorseConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://student-management-frontend-lyart.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
